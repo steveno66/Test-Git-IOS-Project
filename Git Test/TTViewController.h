5540cd29fcbc1f9903374e8f6988a6b7ce7735a5
macbookpro <macbookpro@macbook-pros-macbook-pro.local>
2014-07-28 15:17:50 +0200

    Initial Commit

---
+++ Git Test/TTViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTViewController.h
+//  Git Test
+//
+//  Created by macbookpro on 2014/07/28.
+//  Copyright (c) 2014 Technotopia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTViewController : UIViewController
+
+@end
---
+++ Git Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by macbookpro on 2014/07/28.
+//  Copyright (c) 2014 Technotopia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TTAppDelegate class]));
+    }
+}
