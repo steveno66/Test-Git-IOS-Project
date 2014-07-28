16ec2696a18b224056e6253bf0f3c030654c1ec2
macbookpro <macbookpro@macbook-pros-macbook-pro.local>
2014-07-28 15:40:19 +0200

    added a test class

---
+++ TTTest.h
@@ -0,0 +1,13 @@
+//
+//  TTTest.h
+//  Git Test
+//
+//  Created by macbookpro on 2014/07/28.
+//  Copyright (c) 2014 Technotopia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TTTest : NSObject
+
+@end
---
+++ TTTest.m
@@ -0,0 +1,13 @@
+//
+//  TTTest.m
+//  Git Test
+//
+//  Created by macbookpro on 2014/07/28.
+//  Copyright (c) 2014 Technotopia. All rights reserved.
+//
+
+#import "TTTest.h"
+
+@implementation TTTest
+
+@end
